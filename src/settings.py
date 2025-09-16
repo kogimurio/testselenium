a0effbc61df0781ec35e9727a45262156219ca20
Kenneth <kogimurio@gmail.com>
2025-09-16 09:43:15 +0300

    CREATED: Style job list

--- src/settings.py
+++ src/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Application definition
 
